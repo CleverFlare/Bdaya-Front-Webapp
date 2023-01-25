89e0f92dd15ca17180befe87fd2063281e369806
Flare <blastclever@gmail.com>
2023-01-25 22:53:27 +0200

    fix: the direction on reload

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,18 @@ import { LandingPage } from "./landing-page";
 import { useOnMount } from "./hooks";
 import { Loading } from "./components/atoms/Loading";
 import { ScrollToTop } from "./components/atoms/ScrollToTop";
+import i18next from "i18next";
 
 function App() {
   useOnMount(() => {
+    switch (i18next.language) {
+      case "en":
+        document.body.setAttribute("dir", "ltr");
+        break;
+      case "ar":
+        document.body.setAttribute("dir", "rtl");
+        break;
+    }
     console.log(
       "%cWelcome To Bdaya's Console",
       "padding: 5px; color: white; background: linear-gradient(to right, purple, blue); border-radius: 5px"
